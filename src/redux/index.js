e28b900b7b0bbfb418d26139a1fc26f18835f6ef
Guilherme Decampo <guilherme.decampo@gmail.com>
2018-10-03 14:34:01 -0300

    Update: start full change to monolith

---
+++ src/redux/index.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import waitUntilAction from './waitUntilAction';
+import reducer from '../reducers/combinedReducer';
+
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(thunkMiddleware, waitUntilAction),
+    window.devToolsExtension ? window.devToolsExtension() : f => f,
+  ),
+);
+
+export default store;
